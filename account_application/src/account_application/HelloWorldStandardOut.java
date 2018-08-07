7c06097aa3df51011e8e59ea18e5ccf05f1847f5
ssb91 <sasha.birdi@yahoo.co.uk>
2018-08-07 16:52:21 +0100

    upload

---
+++ account_application/src/account_application/HelloWorldStandardOut.java
@@ -0,0 +1,12 @@
+package account_application;
+
+public class HelloWorldStandardOut {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World to the standard out");
+
+	}		
+			
+	}
+
